6c9322bda335d4abbc4d692546db1ddab89bd847
Wildanzulfikar <wildanzulfikar811@gmail.com>
2025-05-13 19:39:35 +0700

    bere

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,5 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="/js/app.js"></script>
 </body>
 </html>
